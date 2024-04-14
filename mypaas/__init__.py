c1c448c008d420f5f74a4d282a480a3e4a7ec1f2
Lóránt Meszlényi <meszlenyilorant@gmail.com>
2024-04-14 21:14:40 +0200

    Update traefik.

--- mypaas/__init__.py
+++ mypaas/__init__.py
@@ -8,7 +8,7 @@ automatic https (via Let's Encrypt) and deployments via dockerfiles.
 
 
 __version__ = "0.5.9"
-__traefik_version__ = "2.10.0"
+__traefik_version__ = "2.11"
 
 version_info = tuple(map(int, __version__.split(".")))
 
